f42765e60fe3496f5970b215892b742de63daa2e
nainaistar <nainaistar@gmail.com>
2021-05-02 23:35:50 +0900

    [Add] initial

---
+++ src/main/java/kirimaru/practicetestcontainers/PracticeTestContainersApplication.java
@@ -0,0 +1,13 @@
+package kirimaru.practicetestcontainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeTestContainersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeTestContainersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kirimaru/practicetestcontainers/PracticeTestContainersApplicationTests.java
@@ -0,0 +1,13 @@
+package kirimaru.practicetestcontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeTestContainersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
